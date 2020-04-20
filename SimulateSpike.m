74be35b20d12cb5855aac3a4db0fc90e69723d87
Mehrad <59752677+MehradSm@users.noreply.github.com>
2020-04-20 13:59:23 -0400

    Update SimulateSpike.m

--- SimulateSpike.m
+++ SimulateSpike.m
@@ -4,9 +4,7 @@ spiketrain = zeros(nsteps,1);
 for i=lastknot+1:nsteps
    lambda(i) = exp(theta*[1; splinematrix'*spiketrain(i-1:-1:i-lastknot)]);
    spiketrain(i) = min(poissrnd(lambda(i)),1);
-   
 end
-
 ytrue = glmval(theta',splinematrix,'log');
 
 end
