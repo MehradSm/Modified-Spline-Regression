1365b88fcd7f27eb6b5b48b9c25b24cbc94bbd56
Mehrad <59752677+MehradSm@users.noreply.github.com>
2021-07-15 20:17:50 -0400

    Update ModifiedCardinalSpline.m

--- ModifiedCardinalSpline.m
+++ ModifiedCardinalSpline.m
@@ -8,17 +8,23 @@ for i=1:lag
     next_c_pt_time = c_pt(nearest_c_pt_index+1);
     % Compute the fractional distance between timepoint i and the nearest knot
     u = (i-nearest_c_pt_time)./(next_c_pt_time-nearest_c_pt_time);
+    lb = (c_pt(3) - c_pt(1))/(c_pt(2)-c_pt(1));
+    le = (c_pt(end) - c_pt(end-2))/(c_pt(end) - c_pt(end-1));
     % Beginning knot 
     if nearest_c_pt_time==c_pt(1) 
-           p=[u^3 u^2 u 1]*[2-s -2 s;s-3 3 -s;0 0 0;1 0 0];
+           p=[u^3 u^2 u 1]*[2-(s/lb) -2 s/lb;(s/lb)-3 3 -s/lb;0 0 0;1 0 0];
            HistSpl(i,nearest_c_pt_index:nearest_c_pt_index+2) = p; 
     % End knot
     elseif nearest_c_pt_time==c_pt(end-1) 
-           p=[u^3 u^2 u 1]*[-s 2 -2+s;2*s -3 3-2*s;-s 0 s;0 1 0];
+           p=[u^3 u^2 u 1]*[-s/le 2 -2+(s/le);2*s/le -3 3-(2*s/le);-s/le 0 s/le;0 1 0];
            HistSpl(i,nearest_c_pt_index-1:nearest_c_pt_index+1) = p;  
     % Interior knots
     else
-           p=[u^3 u^2 u 1]*[-s 2-s s-2 s;2*s s-3 3-2*s -s;-s 0 s 0;0 1 0 0];
+           privious_c_pt = c_pt(nearest_c_pt_index-1);
+           next2 = c_pt(nearest_c_pt_index+2);
+           l1 = next_c_pt_time - privoius_c_pt;
+           l2 = next2 - nearest_c_pt_time;
+           p=[u^3 u^2 u 1]*[-s/l1 2-(s/l2) (s/l1)-2 s/l2;2*s/l1 (s/l2)-3 3-2*(s/l1) -s/l2;-s/l1 0 s/l1 0;0 1 0 0];
            HistSpl(i,nearest_c_pt_index-1:nearest_c_pt_index+2) = p; 
     end
 end
